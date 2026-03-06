f4504e611b63404d36069770ad4335960bdca87d
Tosin228 <tosin@finamo.biz>
2026-03-06 04:47:37 -0600

    update homepage

--- script.js
+++ script.js
@@ -1,7 +1,31 @@
 const toggle = document.querySelector('.nav-toggle')
 const nav = document.querySelector('.main-nav')
+const navClose = document.querySelector('.nav-close')
+const brand = document.querySelector('.brand')
+
 if (toggle && nav) {
   toggle.addEventListener('click', () => {
     nav.classList.toggle('open')
   })
 }
+
+if (navClose && nav) {
+  navClose.addEventListener('click', () => {
+    nav.classList.remove('open')
+  })
+}
+
+if (brand && nav) {
+  brand.addEventListener('click', (e) => {
+    if (window.innerWidth <= 960) {
+      e.preventDefault()
+      nav.classList.toggle('open')
+    }
+  })
+}
+
+window.addEventListener('load', () => {
+  if (window.innerWidth <= 960) {
+    nav.classList.add('open')
+  }
+})
